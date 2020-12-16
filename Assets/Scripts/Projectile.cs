af262b17e6bb8ee9e8b47688561a24868a13637a
Bui Quang Bao <60503568+buiquangbao@users.noreply.github.com>
2020-12-16 10:51:48 +0700

    Upload

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody2D;
+
+    // Awake is called immediately when the object is created
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // This prevent a lot of cogs move outside the view while game run
+        if(transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2D.AddForce(direction * force);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if(e != null)
+        {
+            e.Fix();
+        }
+        Destroy(gameObject);
+    }
+
+}
